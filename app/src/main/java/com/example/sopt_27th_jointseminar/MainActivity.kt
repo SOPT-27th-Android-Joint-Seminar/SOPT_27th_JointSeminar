9b03338e73babbe586ac9126bd1e3c5102ab6a00
heewon <ymcho24@naver.com>
2020-11-15 04:44:51 +0900

    feat: BottomNavigation 배치

--- app/src/main/java/com/example/sopt_27th_jointseminar/MainActivity.kt
+++ app/src/main/java/com/example/sopt_27th_jointseminar/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.sopt_27th_jointseminar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Theme에 의해 구조적으로 포함되어 있는 Tint 초기화
+        main_bottom_navi.itemIconTintList = null
     }
 }
