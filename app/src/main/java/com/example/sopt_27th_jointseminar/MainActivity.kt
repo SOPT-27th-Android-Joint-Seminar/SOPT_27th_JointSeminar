b047387fc74d9646410ae5829134bd90ca33e7c3
heewon <ymcho24@naver.com>
2020-11-15 06:14:33 +0900

    feat: HomeFragment layout 생성

--- app/src/main/java/com/example/sopt_27th_jointseminar/MainActivity.kt
+++ app/src/main/java/com/example/sopt_27th_jointseminar/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.sopt_27th_jointseminar
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.viewpager.widget.ViewPager
+import com.example.sopt_27th_jointseminar.fragment.*
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.properties.Delegates
 
--- app/src/main/java/com/example/sopt_27th_jointseminar/fragment/PersonFragment.kt
+++ app/src/main/java/com/example/sopt_27th_jointseminar/fragment/PersonFragment.kt
@@ -1,17 +1,18 @@
-package com.example.sopt_27th_jointseminar
+package com.example.sopt_27th_jointseminar.fragment
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.sopt_27th_jointseminar.R
 
-class HomeFragment : Fragment() {
+class PersonFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_person, container, false)
     }
 }
